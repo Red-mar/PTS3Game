f0a76237e47e514ce08467c370d5f737ec2c0d2c
Gijs van den Biggelaar <372192@student.fontys.nl>
2017-09-19 15:01:21 +0200

    You can change colors!

--- PTS3-Game/core/src/com/game/pts3/PTS3Game.java
+++ PTS3-Game/core/src/com/game/pts3/PTS3Game.java
@@ -2,6 +2,7 @@ package com.game.pts3;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,6 +10,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class PTS3Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	float red = 1;
+	float green = 1;
 	
 	@Override
 	public void create () {
@@ -18,11 +21,24 @@ public class PTS3Game extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(red, green, 0.343f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 50, 50);
 		batch.end();
+
+		if(Gdx.input.isKeyPressed(Keys.LEFT) && red > 0.1f){
+			red-= 0.1f;
+		}
+		if(Gdx.input.isKeyPressed(Keys.RIGHT) && red < 0.9f){
+			red+= 0.1f;
+		}
+		if(Gdx.input.isKeyPressed(Keys.DOWN) && green > 0.1f){
+			green-= 0.1f;
+		}
+		if(Gdx.input.isKeyPressed(Keys.UP) && green < 0.9f){
+			green+= 0.1f;
+		}
 	}
 	
 	@Override
