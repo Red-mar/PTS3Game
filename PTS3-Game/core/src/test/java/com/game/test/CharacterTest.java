9e44dd95c5b4184a3f2ed7366688d4e75daa972a
Boekel,Kelvin K. van <k.vanboekel@student.fontys.nl>
2017-09-26 13:53:19 +0200

    Junit4

---
+++ PTS3-Game/core/src/test/java/com/game/test/CharacterTest.java
@@ -0,0 +1,88 @@
+package com.game.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CharacterTest {
+    @Test
+    public void getName() throws Exception {
+    }
+
+    @Test
+    public void setName() throws Exception {
+    }
+
+    @Test
+    public void getMaxHealthPoints() throws Exception {
+    }
+
+    @Test
+    public void setMaxHealthPoints() throws Exception {
+    }
+
+    @Test
+    public void getCurrentHealthPoints() throws Exception {
+    }
+
+    @Test
+    public void setCurrentHealthPoints() throws Exception {
+    }
+
+    @Test
+    public void getAttackPoints() throws Exception {
+    }
+
+    @Test
+    public void setAttackPoints() throws Exception {
+    }
+
+    @Test
+    public void getDefensePoints() throws Exception {
+    }
+
+    @Test
+    public void setDefensePoints() throws Exception {
+    }
+
+    @Test
+    public void getMovementPoints() throws Exception {
+    }
+
+    @Test
+    public void setMovementPoints() throws Exception {
+    }
+
+    @Test
+    public void isDead() throws Exception {
+    }
+
+    @Test
+    public void setDead() throws Exception {
+    }
+
+    @Test
+    public void getCurrentTerrain() throws Exception {
+    }
+
+    @Test
+    public void setCurrentTerrain() throws Exception {
+    }
+
+    @Test
+    public void getPosition() throws Exception {
+    }
+
+    @Test
+    public void setPosition() throws Exception {
+    }
+
+    @Test
+    public void getPlayer() throws Exception {
+    }
+
+    @Test
+    public void setPlayer() throws Exception {
+    }
+
+}
