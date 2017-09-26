ff3733f4fdbd02120179eb6f7006a0b6f36464c6
Boekel,Kelvin K. van <k.vanboekel@student.fontys.nl>
2017-09-26 15:01:39 +0200

    unit charachter, map

--- PTS3-Game/core/src/com/game/classes/unitTest/CharacterTest.java
+++ PTS3-Game/core/src/com/game/classes/unitTest/CharacterTest.java
@@ -1,94 +1,119 @@
 package com.game.classes.unitTest;
 
 import com.game.classes.Character;
+import com.game.classes.Player;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CharacterTest {
+    Character c = new Character("", 10, 20, 30, 40);
 
     @Test
-    public void getName() throws Exception {
-        Character c = new Character("", 10, 20, 30, 40);
+    public void getName() throws Exception
+    {
         assertEquals("", c.getName());
     }
 
     @Test
-    public void setName() throws Exception {
+    public void setName() throws Exception
+    {
+        c.setName("test2");
+        assertEquals("test2", c.getName());
+    }
+
+    @Test
+    public void getMaxHealthPoints() throws Exception
+    {
+        assertEquals(10, c.getMaxHealthPoints());
+    }
+
+    @Test
+    public void setMaxHealthPoints() throws Exception
+    {
+        c.setMaxHealthPoints(15);
+        assertEquals(15, c.getMaxHealthPoints());
+    }
+
+    @Test
+    public void setCurrentHealthPoints() throws Exception
+    {
+        c.setCurrentHealthPoints(5);
+        assertEquals(5, c.getCurrentHealthPoints());
+    }
+
+    @Test
+    public void getAttackPoints() throws Exception
+    {
+        assertEquals(20, c.getAttackPoints());
+    }
+
+    @Test
+    public void setAttackPoints() throws Exception
+    {
+        c.setAttackPoints(25);
+        assertEquals(25, c.getAttackPoints());
+    }
+
+    @Test
+    public void getDefensePoints() throws Exception
+    {
+        assertEquals(30, c.getDefensePoints());
+    }
+
+    @Test
+    public void setDefensePoints() throws Exception
+    {
+        c.setDefensePoints(35);
+        assertEquals(35, c.getDefensePoints());
+    }
+
+    @Test
+    public void getMovementPoints() throws Exception
+    {
+        assertEquals(40, c.getMovementPoints());
+    }
+
+    @Test
+    public void setMovementPoints() throws Exception
+    {
+        c.setMovementPoints(45);
+        assertEquals(45, c.getMovementPoints());
+    }
+
+    @Test
+    public void setDead() throws Exception
+    {
+        c.setDead(true);
+        assertEquals(true, c.isDead());
+    }
+
+    @Test
+    public void getCurrentTerrain() throws Exception
+    {
 
     }
 
     @Test
-    public void getMaxHealthPoints() throws Exception {
+    public void setCurrentTerrain() throws Exception
+    {
 
     }
 
     @Test
-    public void setMaxHealthPoints() throws Exception {
+    public void setPosition() throws Exception
+    {
+        int[] i = {2,2};
+        c.setPosition(i);
+        assertEquals(i, c.getPosition());
     }
 
     @Test
-    public void getCurrentHealthPoints() throws Exception {
-    }
-
-    @Test
-    public void setCurrentHealthPoints() throws Exception {
-    }
-
-    @Test
-    public void getAttackPoints() throws Exception {
-    }
-
-    @Test
-    public void setAttackPoints() throws Exception {
-    }
-
-    @Test
-    public void getDefensePoints() throws Exception {
-    }
-
-    @Test
-    public void setDefensePoints() throws Exception {
-    }
-
-    @Test
-    public void getMovementPoints() throws Exception {
-    }
-
-    @Test
-    public void setMovementPoints() throws Exception {
-    }
-
-    @Test
-    public void isDead() throws Exception {
-    }
-
-    @Test
-    public void setDead() throws Exception {
-    }
-
-    @Test
-    public void getCurrentTerrain() throws Exception {
-    }
-
-    @Test
-    public void setCurrentTerrain() throws Exception {
-    }
-
-    @Test
-    public void getPosition() throws Exception {
-    }
-
-    @Test
-    public void setPosition() throws Exception {
-    }
-
-    @Test
-    public void getPlayer() throws Exception {
-    }
-
-    @Test
-    public void setPlayer() throws Exception {
+    public void setPlayer() throws Exception
+    {
+        Player p = new Player("testPlayer");
+        c.setPlayer(p);
+        assertEquals(p, c.getPlayer());
     }
 
 }
--- PTS3-Game/core/src/com/game/classes/unitTest/MapTest.java
+++ PTS3-Game/core/src/com/game/classes/unitTest/MapTest.java
@@ -1,5 +1,6 @@
 package com.game.classes.unitTest;
 
+import com.game.classes.Map;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,8 +8,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MapTest {
+    Map map;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws Exception
+    {
+        map = new Map(10, 20);
     }
 
     @After
@@ -16,19 +21,29 @@ public class MapTest {
     }
 
     @Test
-    public void getSizeX() throws Exception {
+    public void getSizeX() throws Exception
+    {
+        assertEquals(10, map.getSizeX());
     }
 
     @Test
-    public void setSizeX() throws Exception {
+    public void setSizeX() throws Exception
+    {
+        map.setSizeX(15);
+        assertEquals(15, map.getSizeX());
     }
 
     @Test
-    public void getSizeY() throws Exception {
+    public void getSizeY() throws Exception
+    {
+        assertEquals(20, map.getSizeY());
     }
 
     @Test
-    public void setSizeY() throws Exception {
+    public void setSizeY() throws Exception
+    {
+        map.setSizeY(25);
+        assertEquals(25, map.getSizeY());
     }
 
     @Test
