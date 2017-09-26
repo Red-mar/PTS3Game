34bccadcee463bc7d07e493ee691167de04de35d
Gijs van den Biggelaar <372192@student.fontys.nl>
2017-09-26 11:41:17 +0200

    Chat, Player, Terrain

--- PTS3-Game/core/src/com/game/classes/Chat.java
+++ PTS3-Game/core/src/com/game/classes/Chat.java
@@ -1,4 +1,35 @@
 package com.game.classes;
 
 public class Chat {
+    private Game game;
+    private String message;
+    private String[] chatlog;
+
+    public Chat(){
+        chatlog = new String[50];
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String[] getChatlog() {
+        return chatlog;
+    }
+
+    public void setChatlog(String[] chatlog) {
+        this.chatlog = chatlog;
+    }
 }
