80fa418d1edcd160fcb2fe1e66c676656c5ca7ad
Kmhofstede <kmhofstede@gmail.com>
2017-09-26 12:02:49 +0200

    Implementatie (game class)
    Game class geimplementeerd.

--- PTS3-Game/core/src/com/game/classes/Game.java
+++ PTS3-Game/core/src/com/game/classes/Game.java
@@ -1,4 +1,65 @@
 package com.game.classes;
 
-public class Game {
+import java.util.ArrayList;
+
+public class Game
+{
+    //fields
+    private ArrayList<Player> players;
+    private Map map;
+    private Chat chat;
+    private Boolean inGame;
+
+    //constructor
+    public Game()
+    {
+
+    }
+
+    //methods
+    public ArrayList<Player> getPlayers()
+    {
+        return this.players;
+    }
+
+    public void addPlayer(Player Player)
+    {
+        players.add(Player);
+    }
+
+    public void removePlayer(Player player)
+    {
+        for (Player current:players)
+        {
+            if(current.equals(player))
+            {
+                players.remove(current);
+            }
+        }
+    }
+
+    public Map getMap()
+    {
+        return this.map;
+    }
+
+    public void setMap(Map map)
+    {
+        this.map = map;
+    }
+
+    public void setChat(Chat chat)
+    {
+        this.chat = chat;
+    }
+
+    public boolean getInGame()
+    {
+        return getInGame();
+    }
+
+    public void setInGame(boolean inGame)
+    {
+        this.inGame = inGame;
+    }
 }
