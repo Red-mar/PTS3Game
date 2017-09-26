f831bfd6a6b16b06e9b91fcf8c27ca9e557f5a44
Gijs van den Biggelaar <372192@student.fontys.nl>
2017-09-26 14:04:18 +0200

    JUnit4 fix

--- PTS3-Game/core/src/test/java/com/game/test/PlayerTest.java
+++ PTS3-Game/core/src/test/java/com/game/test/PlayerTest.java
@@ -1,5 +1,54 @@
+package com.game.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PlayerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getName() throws Exception {
+    }
+
+    @Test
+    public void isSpectator() throws Exception {
+    }
+
+    @Test
+    public void setSpectator() throws Exception {
+    }
+
+    @Test
+    public void isReady() throws Exception {
+    }
+
+    @Test
+    public void setReady() throws Exception {
+    }
+
+    @Test
+    public void getCharacters() throws Exception {
+    }
+
+    @Test
+    public void setCharacters() throws Exception {
+    }
+
+    @Test
+    public void isLocalPlayer() throws Exception {
+    }
+
+    @Test
+    public void setLocalPlayer() throws Exception {
+    }
 
 }
