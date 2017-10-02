b180337f516fa6124d1edd0d0acbbf53d1cafa8f
Marouan Bakour <marouan.bakour96@gmail.com>
2017-10-02 12:20:56 +0200

    Added even more assets and made the map load

--- PTS3-Game/core/src/com/game/pts3/ScreenGame.java
+++ PTS3-Game/core/src/com/game/pts3/ScreenGame.java
@@ -4,9 +4,12 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.Map;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -18,11 +21,22 @@ public class ScreenGame implements Screen {
     Stage stage;
     private Skin skin;
     private Game game;
+    private Texture texture;
+    private TiledMap tiledMap;
+    private OrthographicCamera camera;
+    private TiledMapRenderer renderer;
 
 
 
     public ScreenGame(Game game){
         stage = new Stage();
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, width, height);
+        camera.update();
+        tiledMap = new TmxMapLoader().load("map.tmx");
+        renderer = new OrthogonalTiledMapRenderer(tiledMap);
         this.game = game;
         skin = new Skin(Gdx.files.internal("data/uiskin.json"));
 
@@ -42,6 +56,9 @@ public class ScreenGame implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor( 0, 1, 0, 1 );
         Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
 
         stage.act();
         stage.draw();
