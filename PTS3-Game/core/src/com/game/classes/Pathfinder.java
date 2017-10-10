a1e3c8cbca7e325b029ac06f33eb32f066c600e2
Gijs van den Biggelaar <372192@student.fontys.nl>
2017-10-10 19:37:14 +0200

    Some more Pathfinder progress

--- PTS3-Game/core/src/com/game/classes/Pathfinder.java
+++ PTS3-Game/core/src/com/game/classes/Pathfinder.java
@@ -23,6 +23,7 @@ public class Pathfinder implements Serializable {
                     if (map.getTerrains()[terrain.getX()][i].getProperty() == TerrainProperties.Impassable){
                         //impassable terrain found underneath terrain.
                         if (character.getMovementPoints() - 2 >= terrain.getY() - character.getCurrentTerrain().getY()){
+                            //terrain is too far
                             return true;
                         }
                         else{
@@ -37,6 +38,7 @@ public class Pathfinder implements Serializable {
                     if (map.getTerrains()[terrain.getX()][i].getProperty() == TerrainProperties.Impassable){
                         //impassable terrain found above terrain.
                         if (character.getMovementPoints() - 2 >= character.getCurrentTerrain().getY() - terrain.getY()){
+                            //terrain is too far
                             return true;
                         }
                         else{
@@ -49,11 +51,12 @@ public class Pathfinder implements Serializable {
         else if (terrain.getY() == character.getCurrentTerrain().getY()){
             //On same Y as char, either left or right.
             if (terrain.getX() > character.getCurrentTerrain().getX()){
-                //left
+                //right
                 for (int i = terrain.getX() - 1; i > character.getCurrentTerrain().getX(); i--){
                     if (map.getTerrains()[i][terrain.getY()].getProperty() == TerrainProperties.Impassable){
                         //impassable terrain found left of terrain.
                         if (character.getMovementPoints() - 2 >= terrain.getX() - character.getCurrentTerrain().getX()){
+                            //terrain is too far
                             return true;
                         }
                         else{
@@ -63,12 +66,12 @@ public class Pathfinder implements Serializable {
                 }
             }
             else{
-                //right
-
+                //left
                 for (int i = terrain.getX() + 1; i < character.getCurrentTerrain().getX(); i++){
                     if (map.getTerrains()[i][terrain.getY()].getProperty() == TerrainProperties.Impassable){
                         //impassable terrain found right of terrain.
                         if (character.getMovementPoints() - 2 >= character.getCurrentTerrain().getX() - terrain.getX()){
+                            //terrain is too far
                             return true;
                         }
                         else{
@@ -82,18 +85,61 @@ public class Pathfinder implements Serializable {
             //not in the direct LoS of char.
             if (terrain.getY() > character.getCurrentTerrain().getY()){
                 //up
+                for (int i = terrain.getY() - 1; i > character.getCurrentTerrain().getY(); i--){
+                    if (map.getTerrains()[terrain.getX()][i].getProperty() == TerrainProperties.Impassable){
+                        //impassable terrain found underneath terrain.
+                        if (map.getTerrains()[terrain.getX() + 1][i].getProperty() == TerrainProperties.Impassable && checkForLosWithCharacter(map.getTerrains()[terrain.getX() + 1][i], true)){
+                            //terrain to the right is impassable and in line of sight with character.
+                            if (character.getMovementPoints() - 3 >= terrain.getY() - character.getCurrentTerrain().getY()){
+                                //terrain is too far
+                                return true;
+                            }
+                            else{
+                                return false;
+                            }
+                        }
+
+                        if (map.getTerrains()[terrain.getX() - 1][i].getProperty() == TerrainProperties.Impassable && checkForLosWithCharacter(map.getTerrains()[terrain.getX() - 1][i], true)){
+                            //terrain to the left is impassable and in line of sight with character.
+                            if (character.getMovementPoints() - 3 >= terrain.getY() - character.getCurrentTerrain().getY()){
+                                //terrain is too far
+                                return true;
+                            }
+                            else{
+                                return false;
+                            }
+                        }
+
+                        return false;
+                    }
+                }
             }
             else {
                 //down
+                
             }
             if (terrain.getX() > character.getCurrentTerrain().getX()){
-                //left
+                //right
             }
             else{
-                //right
+                //left
             }
         }
         return true;
+    }
+
+    private boolean checkForLosWithCharacter(Terrain terrainFound, boolean vertical){
+        if (vertical){
+            if (terrainFound.getX() == character.getCurrentTerrain().getX()){
+                return true;
+            }
+        }
+        else {
+            if (terrainFound.getY() == character.getCurrentTerrain().getY()){
+                return true;
+            }
+        }
+        return false;
     }
 
     public void setMap(Map map){
