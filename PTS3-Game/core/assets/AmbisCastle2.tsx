859e51b916a2b54e78286ffa9035773c09525e9c
Gijs van den Biggelaar <372192@student.fontys.nl>
2017-11-14 13:22:54 +0100

    Added new map: castle.tmx

---
+++ PTS3-Game/core/assets/AmbisCastle2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Ambis-Castle2" tilewidth="16" tileheight="16" tilecount="242" columns="22">
+ <image source="C:/Users/user/Downloads/Ambis-Castle2.png" width="352" height="176"/>
+</tileset>
