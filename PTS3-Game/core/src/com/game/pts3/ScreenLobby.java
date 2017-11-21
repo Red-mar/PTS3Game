342a231c91218ed3f408301156af1e96d08245c1
Riet,Redmar R.F. van de <r.vanderiet@student.fontys.nl>
2017-11-21 10:21:46 +0100

    improved join

--- PTS3-Game/core/src/com/game/pts3/ScreenLobby.java
+++ PTS3-Game/core/src/com/game/pts3/ScreenLobby.java
@@ -267,7 +267,7 @@ public class ScreenLobby implements Screen, GameEvents {
                 manager.get("bgm/bgmbase1.mp3", Music.class).stop();
                 chat.getTextArea().appendText("Starting the game...\n");
 
-                Timer timer = new Timer();
+                final Timer timer = new Timer();
                 Timer.Task task = new Timer.Task() {
                     @Override
                     public void run() {
@@ -277,6 +277,7 @@ public class ScreenLobby implements Screen, GameEvents {
 
                         game.setScreen(new ScreenGame(game, gameState, chat, manager));
                         stage.clear();
+                        this.cancel();
                     }
                 };
                 timer.scheduleTask(task, 2f);
@@ -354,6 +355,13 @@ public class ScreenLobby implements Screen, GameEvents {
         for (Player player:gameState.getPlayers()) {
             if (!player.isReady()) {
                 chat.textArea.appendText("Niet iedereen is READY.\n");
+                return;
+            }
+            if (player.getCharacters().size() > 0){
+                gameState.getClient().sendGameJoin();
+                if (!gameState.getClientPlayer().isSpectator()){
+                    gameState.generateCharacters(gameState.getClientPlayer().getName(), manager);
+                }
                 return;
             }
         }
