141823d08f5c47492c8325cea1b884f8851d9f5d
Gijs van den Biggelaar <g.vandenbiggelaar@gmail.com>
2017-11-21 14:43:40 +0100

    Changed application title

--- PTS3-Game/desktop/src/com/game/pts3/desktop/DesktopLauncher.java
+++ PTS3-Game/desktop/src/com/game/pts3/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.game.pts3.StageStart;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "PTS3-Game!";
+		config.title = "GridScape: Online";
 		config.width = 1280;
 		config.height = 720;
 		new LwjglApplication(new StageStart(), config);
