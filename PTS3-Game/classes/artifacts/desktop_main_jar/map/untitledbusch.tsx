e1f0ff9ec25cdc893fbc344651432b7f9ebb886c
Riet,Redmar R.F. van de <r.vanderiet@student.fontys.nl>
2017-12-19 10:49:07 +0100

    artifacts verwijderd

--- PTS3-Game/classes/artifacts/desktop_main_jar/map/untitledbusch.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="buch-outdoor" tilewidth="15" tileheight="15" spacing="1" margin="1" tilecount="288" columns="24">
- <image source="buch-outdoor.png" width="384" height="192"/>
-</tileset>
